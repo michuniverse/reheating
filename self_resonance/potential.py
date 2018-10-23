1b95e29dab930319a949917f93c60d2e349b20da
Supranta <ssarmabo@uwaterloo.ca>
2018-10-23 13:17:48 +0200

    added tests for the time integration of the systems of equations

--- self_resonance/potential.py
+++ self_resonance/potential.py
@@ -1,19 +1,37 @@
 import numpy as np
-from scipy.integrate import quadrature
+from scipy.integrate import quadrature, odeint
 
 class Potential:
     def __init__(self):
         #blablabla
         return None
 
-    def potential(self, phi):
+    def V(self, phi):
         return np.sqrt(1.0 + phi**2) - 1.0
 
+    def Vdd(self, phi):
+        return 1.0/np.sqrt(1.0+phi**2)**(3)
+
     def period(self, phimax):
-        _phimin = phimin()
-        f = lambda phi : np.sqrt(2.0/(potential(phimax)-potential(phi)))
+        _phimin = self.phimin()
+        f = lambda phi : np.sqrt(2.0/(self.V(phimax)-self.V(phi)))
         T = quadrature(f, _phimin, phimax)
-        return T
+        return T[0]
 
     def phimin(self):
         return 0.0
+
+    def system_equations(self, x, t, k):
+        phi, pi = x
+
+        _vdd = self.Vdd(phi)
+
+        phi_dot = pi
+        pi_dot = -(k**2 + _vdd)*phi
+
+        return [phi_dot, pi_dot]
+
+    def integrate(self, x0, T, k, N_steps=5000):
+        t = np.linspace(0.0, T, N_steps)
+        x = odeint(self.system_equations, x0, t, args=(k,))
+        return x
---
+++ self_resonance/tests/test_time_integration.py
@@ -0,0 +1,37 @@
+import sys, os
+pkg_path = os.path.abspath('..')
+sys.path.append(pkg_path)
+
+import potential as pot
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+Pot = pot.Potential()
+
+phimax = 1.0
+T = Pot.period(phimax)
+print("The time period for phimax=" + str(phimax) + " is " + str(T) + "\n")
+
+k = 1.0
+
+x01 = [1.0, 0]
+y1 = Pot.integrate(x01, T, k)
+print("Integration successful for first initial conditions")
+
+x02 = [0.0, 1.0]
+y2 = Pot.integrate(x02, T, k)
+print("Integration successful for second initial conditions")
+
+t_plot = np.linspace(0.0, T, 5000)
+
+plt.plot(t_plot, y1[:,0])
+plt.savefig('phi_1.png',dpi=150)
+plt.close()
+
+plt.plot(t_plot, y2[:,0])
+plt.savefig('phi_2.png',dpi=150)
+plt.close()
