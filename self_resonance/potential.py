0faf567da619612393c7691cd9248900c20c8f0b
Supranta <ssarmabo@uwaterloo.ca>
2018-10-23 12:16:35 +0200

    Fixes to potential.py and test. Added __init__ for discovering package. Initial test for potential working

--- self_resonance/potential.py
+++ self_resonance/potential.py
@@ -4,15 +4,16 @@ from scipy.integrate import quadrature
 class Potential:
     def __init__(self):
         #blablabla
+        return None
 
-    def potential(phi):
+    def potential(self, phi):
         return np.sqrt(1.0 + phi**2) - 1.0
 
-    def period(phimax):
+    def period(self, phimax):
         _phimin = phimin()
         f = lambda phi : np.sqrt(2.0/(potential(phimax)-potential(phi)))
         T = quadrature(f, _phimin, phimax)
         return T
 
-    def phimin():
+    def phimin(self):
         return 0.0
--- self_resonance/tests/test_potential.py
+++ self_resonance/tests/test_potential.py
@@ -1,21 +1,25 @@
 import os, sys
 
-sys.path.append(os.path.abspath('/..'))
+pkg_path = os.path.abspath('..')
+sys.path.append(pkg_path)
 
 import potential as pot
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.rc('text', usetex=true)
+plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
-if(pot.phimin == 0.0):
-    print('Test Phimin Okay')
+pot = pot.Potential()
+
+if(pot.phimin() == 0.0):
+    print("Test Phimin Okay")
 else:
+    print("Test phimin Failed")
     #throw TestFail exception
 
-plot_phi = np.linspace(-5.0, 5.0, 10000)
+plot_phi = np.linspace(-3.0, 3.0, 10000)
 
 plt.xlabel('$\phi$')
 plt.ylabel('$V(\phi)$')
